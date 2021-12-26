0f1fce06a7d06fdf8512c9cbae8f4e81dcc8b33c
Daulet <sametbay.i5@gmail.com>
2021-12-27 00:03:29 +0600

    VERSION 2.0

--- index.php
+++ index.php
@@ -23,12 +23,12 @@ else
 </head>
 <body>
     <section class="congratulations Daulet You Did it ">
-        <h1>Congratulations!</h1>
+        <h1>Congratulations! <font color=green>Daulet You Did it</font></h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
-    <img src=”1.jpg”>
+    
 
     <section class="instructions">
         <h2>What's Next?</h2>
@@ -46,6 +46,8 @@ else
             <li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
             <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
         </ul>
+        <h2>Picture</h2>
+        <img src="1.jpg">
     </section>
 
     <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
